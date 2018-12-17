8dd264f6fd69416eca0164d678e0fb260296536b
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-12-17 15:15:44 +0100

    Simpler setup.py and remove duplicate __init__.py

--- setup.py
+++ setup.py
@@ -1,79 +1,26 @@
 #!/usr/bin/env python
 
-import re
 import os
 import sys
 from setuptools import setup
 
-name = "django-jwt-auth"
-package = "jwt_auth"
-description = "JSON Web Token based authentication for Django"
-url = "https://github.com/jpadilla/django-jwt-auth"
-author = "Jose Padilla"
-author_email = "hello@jpadilla.com"
-license = "MIT"
-
-
-def get_version(package):
-    """
-    Return package version as listed in `__version__` in `init.py`.
-    """
-    init_py = open(os.path.join(package, "__init__.py")).read()
-    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(
-        1
-    )
-
-
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [
-        dirpath
-        for dirpath, dirnames, filenames in os.walk(package)
-        if os.path.exists(os.path.join(dirpath, "__init__.py"))
-    ]
-
-
-def get_package_data(package):
-    """
-    Return all files under the root package, that are not in a
-    package themselves.
-    """
-    walk = [
-        (dirpath.replace(package + os.sep, "", 1), filenames)
-        for dirpath, dirnames, filenames in os.walk(package)
-        if not os.path.exists(os.path.join(dirpath, "__init__.py"))
-    ]
-
-    filepaths = []
-    for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename) for filename in filenames])
-    return {package: filepaths}
-
-
-version = get_version(package)
-
-
 if sys.argv[-1] == "publish":
     os.system("python setup.py sdist upload")
     os.system("python setup.py bdist_wheel upload")
-    print("You probably want to also tag the version now:")
-    print("  git tag -a {0} -m 'version {0}'".format(version))
-    print("  git push --tags")
+    print("You probably want to also tag the version now...")
     sys.exit()
 
 
 setup(
-    name=name,
-    version=version,
-    url=url,
-    license=license,
-    description=description,
-    author=author,
-    author_email=author_email,
-    packages=get_packages(package),
-    package_data=get_package_data(package),
+    name="django-jwt-auth",
+    version="0.1.0",
+    url="https://github.com/webstack/django-jwt-auth",
+    license="MIT",
+    description="JSON Web Token based authentication for Django",
+    # Original author is "Jose Padilla <hello@jpadilla.com>"
+    author="Stéphane Raimbault",
+    author_email="stephane.raimbault@webstack.fr",
+    packages=["jwt_auth"],
     test_suite="runtests.run_tests",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
