cde371473d9c034a2bffc7f0bb3d2e4a84aa8588
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-12-17 10:34:45 +0100

    Migrate to Django 2

--- tests/urls.py
+++ tests/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import patterns
+from django.urls import path
 
+from jwt_auth import views
 from tests.views import MockView
 
 
-urlpatterns = patterns(
-    '',
-    (r'^jwt/$', MockView.as_view()),
-    (r'^auth-token/$', 'jwt_auth.views.obtain_jwt_token'),
-    (r'^refresh-token/$', 'jwt_auth.views.refresh_jwt_token')
-)
+urlpatterns = [
+    path("jwt/", MockView.as_view()),
+    path("auth-token/", views.obtain_jwt_token),
+    path("refresh-token/", views.refresh_jwt_token)
+]
