884b80ad65b945ee2b471cf402c5f47f4f9cd592
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2018-12-17 10:52:50 +0100

    Remove useless settings in tests

--- tests/settings.py
+++ tests/settings.py
@@ -1,9 +1,7 @@
 DEBUG = False
-TEMPLATE_DEBUG = DEBUG
 
 TIME_ZONE = 'UTC'
 LANGUAGE_CODE = 'en-US'
-SITE_ID = 1
 USE_L10N = True
 USE_TZ = True
 
@@ -18,30 +16,6 @@ DATABASES = {
     }
 }
 
-MIDDLEWARE_CLASSES = [
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-TEMPLATE_CONTEXT_PROCESSORS = [
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    'django.core.context_processors.tz',
-    'django.core.context_processors.request',
-    'django.contrib.messages.context_processors.messages'
-]
-
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
-
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,7 +23,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-
     'tests',
 )
 
