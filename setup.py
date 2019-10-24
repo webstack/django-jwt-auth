18f3e6fd00ee60a7960ee1db54c37e7dd47323b3
Stéphane Raimbault <stephane.raimbault@webstack.fr>
2019-10-24 10:30:19 +0200

    Bump version: 1.1.2 → 1.2.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_long_description():
 
 setup(
     name="webstack-django-jwt-auth",
-    version="1.1.2",
+    version="1.2.0",
     url="https://github.com/webstack/django-jwt-auth",
     license="MIT",
     description="JSON Web Token based authentication for Django",
